a63ebde5c099dc350479244ca4936b144d727fd0
robin <dequanzh@usc.edu>
2021-04-22 21:53:51 -0700

    improve the performance by memorize the moves and less states (9+9+9) instead of options for 81 spot, -49.7% time, +99% through put

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ use std::fmt::Formatter;
 
 pub struct Sudoku {
     board: Vec<u8>,
+    rows: Vec<u16>,
+    cols: Vec<u16>,
+    squares: Vec<u16>,
 }
 
 impl Sudoku {
@@ -13,8 +16,62 @@ impl Sudoku {
             .chars()
             .take(81)
             .map(|c| c.to_digit(10).unwrap() as u8)
-            .collect();
-        Sudoku { board }
+            .collect::<Vec<u8>>();
+        let mut rows = vec![0; 9];
+        let mut cols = vec![0; 9];
+        let mut squares = vec![0; 9];
+
+        // bitmap spot on set means that option is available
+        // for memorization, we only need 9 rows, 9 cols, and 9 squares to check available moves
+        for row in 0..9 {
+            let mut taken_nums_in_row = 0;
+            for i in board
+                .iter()
+                .skip(row as usize * 9)
+                .take(9)
+                .filter(|i| **i != 0)
+            {
+                taken_nums_in_row |= 1 << (i - 1);
+            }
+            rows[row] = !taken_nums_in_row;
+        }
+        for col in 0..9 {
+            let mut taken_nums_in_col = 0;
+            for i in board
+                .iter()
+                .skip(col as usize)
+                .step_by(9)
+                .filter(|i| **i != 0)
+            {
+                taken_nums_in_col |= 1 << (i - 1);
+            }
+            cols[col] = !taken_nums_in_col;
+        }
+
+        for idx in 0..9 {
+            let row = idx/3*3;
+            let col = idx%3*3;
+            let mut taken_nums_in_square = 0;
+            let start = (row / 3 * 27 + col / 3 * 3) as usize;
+            [
+                &board[start..start + 3],
+                &board[start + 9..start + 12],
+                &board[start + 18..start + 21],
+            ]
+            .iter()
+            .map(|slice| slice.iter())
+            .flatten()
+            .filter(|i| **i != 0)
+            .for_each(|i| taken_nums_in_square |= 1 << (i - 1));
+            squares[idx] = !taken_nums_in_square;
+        }
+
+        Sudoku {
+            board,
+            rows,
+            cols,
+            squares,
+        }
     }
 }
 
@@ -50,17 +107,27 @@ pub fn solve_sudoku(game: &mut Sudoku) -> bool {
     match find_first_empty_spot(game) {
         None => true,
         Some((row, col)) => {
-            let bitset = options_for(game, row, col);
-            for option in (1..=9) {
-                if bitset & (1 << (option - 1)) != 0 {
-                    let idx = (row * 9 + col) as usize;
-                    // remove_option_for(game, row, col, option);
+            let row = row as usize;
+            let col = col as usize;
+            let idx = row * 9 + col;
+            let square = row / 3 * 3 + col / 3;
+            let bitset = game.rows[row] & game.cols[col] & game.squares[square];
+            for option in 1..=9 {
+                let mask = 1 << (option - 1);
+                if bitset & mask != 0 {
+                    // use the option by clear
+                    game.rows[row] &= !mask;
+                    game.cols[col] &= !mask;
+                    game.squares[square] &= !mask;
                     game.board[idx] = option;
                     if solve_sudoku(game) {
                         return true;
                     }
-                    // add_option_for(game, row, col, option);
                     game.board[idx] = 0;
+                    // unuse the option by set
+                    game.rows[row] |= mask;
+                    game.cols[col] |= mask;
+                    game.squares[square] |= mask;
                 }
             }
             false
@@ -128,45 +195,3 @@ fn options_for_with_hashset(game: &mut Sudoku, row: u8, col: u8) -> Vec<u8> {
 
     free_num_in_row
 }
-
-fn options_for(game: &mut Sudoku, row: u8, col: u8) -> u16 {
-    let idx = (row * 9 + col) as usize;
-
-    let mut taken_nums_in_row = 0;
-    for i in game
-        .board
-        .iter()
-        .skip(row as usize * 9)
-        .take(9)
-        .filter(|i| **i != 0)
-    {
-        taken_nums_in_row |= 1 << (i - 1);
-    }
-
-    let mut taken_nums_in_col = 0;
-    for i in game
-        .board
-        .iter()
-        .skip(col as usize)
-        .step_by(9)
-        .filter(|i| **i != 0)
-    {
-        taken_nums_in_col |= 1 << (i - 1);
-    }
-
-    let mut taken_nums_in_square = 0;
-    let start = (row / 3 * 27 + col / 3 * 3) as usize;
-    [
-        &game.board[start..start + 3],
-        &game.board[start + 9..start + 12],
-        &game.board[start + 18..start + 21],
-    ]
-    .iter()
-    .map(|slice| slice.iter())
-    .flatten()
-    .filter(|i| **i != 0)
-    .for_each(|i| taken_nums_in_square |= 1 << (i - 1));
-
-    !(taken_nums_in_square | taken_nums_in_col | taken_nums_in_row)
-}
-
